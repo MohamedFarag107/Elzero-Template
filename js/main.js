7fa5116b143035a504731433014fab8c6d35d687
Mohamed <mf1456@fayoum.edu.eg>
2022-07-04 00:27:43 +0200

    scroll to top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+let scrollToTop = document.getElementById("scroll-to-top")
+scrollToTop.onclick = ()=> {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+window.addEventListener("scroll", function () {
+  let top = this.scrollY;
+  console.log(top);
+  if(top>715){
+    scrollToTop.style.right = '0px'
+  }
+  else{
+    scrollToTop.style.right = '-100px'
+  }
+});
+
+
 let otherLinks = document.getElementById("other-links");
 let megaMenu = document.querySelector(".mega-menu");
 otherLinks.onclick = () => {
